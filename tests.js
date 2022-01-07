85b578856a4f365b56dd8baee8e1ed5f7d847a5a
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-01-07 12:33:13 +0000

    added extra test

--- tests.js
+++ tests.js
@@ -4,5 +4,6 @@ const expect = chai.expect
 describe('getCelsius', () => {
     it('should convert fahrenheit to celsius for all values in an array', () => {
         expect(getCelsius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
+        expect(getCelsius([-58, -22, -4, 14])).to.deep.equal([-50, -30, -20, -10])
     })
 })
